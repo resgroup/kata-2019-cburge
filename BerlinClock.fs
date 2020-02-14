5917a8b7a3e098085d67546d48bb3fe8de836033
ceddlyburge <ceddlyburge@gmail.com>
2020-02-14 09:44:27 +0000

    Parse string in to hours minutes and seconds

---
+++ BerlinClock.fs
@@ -0,0 +1,14 @@
+﻿module BerlinClock
+
+open FParsec
+
+let toBerlinClockTime2 hours minutes seconds =
+    seconds
+
+let toBerlinClockTime (julianTime : string) =
+    let parser = pipe3 pint32 (pstring ":" >>. pint32) (pstring ":" >>. pint32) toBerlinClockTime2
+    let parseResult = run parser julianTime
+    match parseResult with
+    | Success(result, _, _)   -> result
+    | Failure(_, _, _) -> -1
+
--- test-kata-2019-cedd/Tests.fs
+++ test-kata-2019-cedd/Tests.fs
@@ -4,6 +4,12 @@ open System
 open Xunit
 open Swensen.Unquote
 open FsCheck
+open BerlinClock
+
+[<Fact>]
+let ``Seconds light is on for 12:40:01``() = 
+   toBerlinClockTime "12:40:01" = 1
+
 
 // double backticks allow us to put spaces in names, which makes the test names clear / more like english / better when you see the test output
 [<Fact>]
@@ -16,13 +22,14 @@ let ``2 + 2 is 4``() =
    let result = 2 + 2
    result =! 4
 
-let add(number1:int, number2:int) = 
+
+let add number1 number2 = 
     number1 + number2
 
 // property based testing will test with a lot of different numbers
 [<Fact>]
 let ``Adding two numbers returns sum of the numbers``() = 
-   let checkadd(number1, number2) =
-    add(number1, number2) = number1 + number2
+   let checkadd number1 number2 =
+    add number1 number2 = number1 + number2
 
    Check.QuickThrowOnFailure checkadd
